ee8824ecda1badc49483d90582fcab56e71a5926
Aswini R M <rm.aswini2005_bit27@mepcoeng.ac.in>
2026-04-01 12:27:41 +0530

    Create Code1.java
    A simple Java program that simulates the result of swapping two integer values. This implementation focuses on achieving the correct final output by reordering the variables during the print statement, bypassing the need for a third temporary variable or complex arithmetic.
    
    Input format :
    The input consists of two space-separated integers, a and b, representing the values to be swapped.
    
    Output format :
    The output prints the two space-separated integers representing the swapped values of a and b.

---
+++ IamNeo/Practice Assessment/Operators_PA_L1/Code1.java
@@ -0,0 +1,13 @@
+// You are using Java
+import java.io.*;
+import java.util.*;
+class swap
+{
+  public static void main(String[] args)
+  {
+      Scanner sc = new Scanner(System.in);
+      int a= sc.nextInt();
+      int b= sc.nextInt();
+      System.out.print(b +" "+a);
+  }
+}
