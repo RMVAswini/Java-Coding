cf233af74b460e83ae48b13896dbe29b7544d7bf
Aswini R M <rm.aswini2005_bit27@mepcoeng.ac.in>
2026-04-01 12:13:36 +0530

    Create Code3.java
    This challenge focuses on handling different data types—Strings and Integers—sequentially using the Scanner class to generate a personalized output message.
    
    Input format :
    The first line of input consists of a string representing the name of the person.
    
    The second line consists of an integer representing the person's age.
    
    Output format :
    The output prints "Age of <name> is <age>", where <name> is the person's name and <age> is their age.

---
+++ IamNeo/Practice Assessment/DataTypes_IO_PA_L1/Code3.java
@@ -0,0 +1,13 @@
+// You are using Java
+import java.io.*;
+import java.util.*;
+class Age
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String name=sc.nextLine();
+        int age =sc.nextInt();
+        System.out.println("Age of "+name+" "+ "is "+age);
+    }
+}
