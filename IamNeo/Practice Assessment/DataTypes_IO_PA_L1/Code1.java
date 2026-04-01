aca5075bb93d6a2d0edd552015c62d890b13276a
Aswini R M <rm.aswini2005_bit27@mepcoeng.ac.in>
2026-04-01 12:10:28 +0530

    Create Code1.java
    Calculates the sum of two integer expenses and formats the result as a double. This project focuses on type promotion (converting int to float/double) and basic Scanner-based input handling.

---
+++ IamNeo/Practice Assessment/DataTypes_IO_PA_L1/Code1.java
@@ -0,0 +1,15 @@
+// You are using Java
+import java.util.*;
+import java.io.*;
+class Sum
+{
+    public static void main(String[] args)
+    {
+      int x,y;
+      Scanner sc = new Scanner(System.in);
+      x=sc.nextInt();
+      y=sc.nextInt();
+      float sum=x+y;
+      System.out.println(sum);
+    }
+}
