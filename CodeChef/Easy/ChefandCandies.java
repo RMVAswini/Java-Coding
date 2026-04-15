e00ed4ff4c002d19b27138d16b9d97ac48b4772d
Aswini R M <rm.aswini2005_bit27@mepcoeng.ac.in>
2026-04-15 11:57:49 +0530

    Create ChefandCandies.java
     This program calculates the minimum number of 4-candy packets Chef needs to purchase to ensure every child in a group of N  receives at least one candy, accounting for the X candies he already possesses.
    
    Input Format
    The first line of input will contain a single integer T, denoting the number of test cases.
    The first and only line of each test case contains two integers N and X — the number of children and the number of candies Chef already has.
    
    Output Format
    For each test case, output the minimum number of candy packets Chef must buy so that he is able to give 1 candy to each of the 
    N children.
    
    Input
    4
    20 12
    10 100
    10 9
    20 9
    
    Output
    2
    0
    1
    3

---
+++ CodeChef/Easy/ChefandCandies.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc  = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-->0)
+		{
+		    int N = sc.nextInt();
+		    int X = sc.nextInt();
+		    if(N>X)
+		    {
+		         int pack = (int) Math.ceil((double)(N-X)/4);
+		         System.out.println(pack);
+		    }
+		    else{
+		        System.out.println("0");
+		    }
+		}
+	}
+}
