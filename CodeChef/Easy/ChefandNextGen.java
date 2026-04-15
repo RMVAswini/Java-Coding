c514d9803c2f7d4e16f9d874355fc5622f02df50
Aswini R M <rm.aswini2005_bit27@mepcoeng.ac.in>
2026-04-15 11:52:24 +0530

    Create ChefandNextGen.java
    Input Format
    The first line of input contains an integer T, the number of testcases. The description of T test cases follows.
    Each test case consists of a single line of input, containing four space-separated integers A,B,X,Y respectively.
    
    Output Format
    For each test case print on a single line the answer — Yes if NEXTGEN satisfies the government's minimum requirements for funding and No otherwise.
    
    You may print each character of the answer string in either uppercase or lowercase (for example, the strings "yEs", "yes", "Yes" and "YES" will all be treated as identical).
    
    Input
    4
    1 2 3 4
    4 3 2 1
    2 18 9 4
    1 100 2 49
    
    Output
    Yes
    No
    Yes
    No

---
+++ CodeChef/Easy/ChefandNextGen.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-->0)
+		{
+		    int A = sc.nextInt();
+		    int B  = sc.nextInt();
+		    int X = sc.nextInt();
+		    int Y = sc.nextInt();
+		    int total = A*B;
+		    int cap = X*Y;
+		    if(cap>=total)
+		    {
+		        System.out.println("Yes");
+		    }
+		    else
+		    {
+		        System.out.println("No");
+		    }
+		}
+	}
+}
