df962c23e4ca88b9125dec92b2d9ed5515754c6e
Aswini R M <rm.aswini2005_bit27@mepcoeng.ac.in>
2026-04-17 20:48:16 +0530

    Create DiscusThrow.java
    To identify the largest value among three given integers: A,B,C
    
    Input Format
    First line will contain T, number of test cases. Then the test cases follow.
    Each test case contains of a single line of input, three integers A,B, and C denoting the distances in each throw.
    
    Output Format
    For each test case, output the final score of the player.
    
    Input
    3
    10 15 8
    32 32 32
    82 45 54
    
    Output
    15
    32
    82

---
+++ CodeChef/Easy/DiscusThrow.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-->0)
+		{
+		    int A = sc.nextInt();
+		    int B = sc.nextInt();
+		    int C = sc.nextInt();
+		    int score = Math.max(A,Math.max(B,C));
+		    System.out.println(score);
+		}
+	}
+}
